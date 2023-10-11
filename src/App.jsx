187d18610bca364764f0cc749e5ea9c336c3c205
Robin Fantino <robin.fantino.rf@gmail.com>
2023-10-11 17:18:22 +0200

    I just finish useStat 1 for the change of the image with next and previous

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard"
 
 function App() {
@@ -9,16 +10,45 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
 
-  const pokemon = pokemonList[0]
+
+
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const HandleClickPrecedent = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+
+  const HandleClicksuivant = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+  const pokemon = pokemonList[pokemonIndex];
 
 
   return (
     <>
       <PokemonCard pokemon={pokemon} />
+      {pokemonIndex > 0 ? <button onClick={HandleClickPrecedent} >Précedent</button> : ''}
+      {pokemonIndex < (pokemonList.length - 1) ? < button onClick={HandleClicksuivant} >Suivant</button > : ''}
     </>
   )
 }
